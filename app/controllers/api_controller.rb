4c4f63346e3fbcf09fec0a1f609cca1c47592654
Alejandro Virlán Burgos <avirlanbrg@gmail.com>
2019-11-18 08:33:41 +0100

    Integración de la columna order, agregación de funciones en la API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,15 +1,48 @@
 class ApiController < ApplicationController
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  prepend_before_action :restrict_access
+
   Apicasso::Key.create(scope:
                            { read:
                                  {
-                                     incidence: true,
-                                     token: true
+                                   incidence: true,
+                                   phone_identifier: true,
+                                   interest_point: true,
+                                   incidence_type: true,
+                                   incidence_tracking: true
                                  },
                              create:
                                  {
-                                     incidence: true,
-                                     token: true
+                                   incidence: true,
+                                   phone_identifier: true
                                  }
                            })
+
+
+  def show_incidences_historical
+    render json: {entries: Incidence.find_by_sql(["SELECT inc.id, inc.image_url, inc.description, inc.latitude, inc.longitude, inc.created_at, it.status, ityp.name
+                                                   FROM incidences inc, (SELECT id, status, date, incidence_id
+                                                                         FROM incidence_trackings it1
+                                                                         WHERE date = (SELECT MAX(date)
+                                                                                       FROM incidence_trackings it2
+                                                                                       WHERE it1.incidence_id = it2.incidence_id)
+                                                                         UNION ALL
+                                                                         SELECT DISTINCT CAST(NULL AS bigint) as id, 1 as status, CAST(NULL AS timestamp) as date, inc.id as incidence_id
+                                                                         FROM incidence_trackings it, incidences inc
+                                                                         WHERE inc.id != it.incidence_id AND NOT EXISTS(SELECT id FROM incidence_trackings
+                                                                                                                        WHERE incidence_id = inc.id)) it, incidence_types ityp
+                                                   WHERE inc.id = it.incidence_id
+                                                   AND inc.incidence_type_id = ityp.id
+                                                   AND inc.phone_identifier_id = ?", PhoneIdentifier.find(params[:id]).id])}
   end
+
+  private
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, _options|
+      @api_key = Apicasso::Key.find_by(token: token)
+    end
+  end
+
+end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :staffs
-  get 'demo/index'
+  get 'demo/index', to: 'demo#index', as: 'demo'
   root to: 'demo#index'
 
+  get 'api/v1/incidences_historical/:id', to: 'api#show_incidences_historical'
   mount Apicasso::Engine, at: "/api/v1"
-
-
 
 end
