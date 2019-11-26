8ef5f0aec59c94ddbfafbe5a82af3c155dbd4a93
Pedro Ríos Flores <periflo90@gmail.com>
2019-11-26 09:15:07 +0100

    Vistas del panel de administración de los usuarios implementadas.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'demo/index', to: 'demo#index', as: 'demo'
   root to: 'demo#index'
 
+  resources :staffs, except: [:show]
+
   get 'api/v1/incidences_historical/:id', to: 'api#show_incidences_historical'
   mount Apicasso::Engine, at: "/api/v1"
 end
