46cacfeb8cf28250e1784ed128f25f99601cc081
Pedro Ríos Flores <periflo90@gmail.com>
2019-11-29 10:05:16 +0100

    Añadido esqueleto de las incidencias.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     resources :processing_units, except: [:show]
     resources :incidence_trackings, except: [:show]
     resources :incidence_types, except: [:show]
+    get 'incidences/index'
+    get 'incidences/show'
   # end
 
   get 'api/v1/incidences_historical/:id', to: 'api#show_incidences_historical'
